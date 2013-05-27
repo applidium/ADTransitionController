4ce8682a9b8c58078ae454d0bfd6e3a50d7df833
Pierre Felgines <pierre.felgines@applidium.com>
2013-05-27 18:33:17 +0200

    Change xib

--- TransitionDemo/Classes/ADAppDelegate.m
+++ TransitionDemo/Classes/ADAppDelegate.m
@@ -19,7 +19,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-    // Override point for customization after application launch.
     
     ADTransitionController * transitionController = [[ADTransitionController alloc] init];
     ALTransitionTestViewController * viewController = [[ALTransitionTestViewController alloc] initWithNibName:nil bundle:nil];
