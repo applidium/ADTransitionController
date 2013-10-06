ad66aabde7baa545d6fb234fd746e79d7b4be836
Martin Guillon <martin.guillon@gmail.com>
2013-10-06 14:31:36 +0200

    New transition to simulate ios7 native transition

---
+++ ADTransitionController/ADIOS7Transition.h
@@ -0,0 +1,14 @@
+//
+//  ADIOS7Transition.h
+//  Titanium
+//
+//  Created by Martin Guillon on 23/09/13.
+//
+//
+
+#import "ADDualTransition.h"
+
+@interface ADIOS7Transition : ADDualTransition
+- (id)initWithDuration:(CFTimeInterval)duration orientation:(ADTransitionOrientation)orientation sourceRect:(CGRect)sourceRect;
+
+@end
