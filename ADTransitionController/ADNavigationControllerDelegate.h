a541d4c0813a4e9f711df854227de2ec9fdddbdf
Nikolay Derkach <nderk@me.com>
2015-05-07 10:32:28 +0700

    Fix UINavigationControllerDelegate not declared

--- ADTransitionController/ADNavigationControllerDelegate.h
+++ ADTransitionController/ADNavigationControllerDelegate.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Applidium. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface ADNavigationControllerDelegate : NSObject <UINavigationControllerDelegate>
 
